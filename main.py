2e078e5faff09f9306e50c1b97fbb8c413282892
Kent <winkrislase@gmail.com>
2025-03-11 07:37:59 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,66 +1,75 @@
-# Session 2
-FirstName = input("First Name: Insert Your First Name")
-MiddleName = input("Middle Name: Insert Your Middle Name")
-LastName = input("Last Name: Insert Your Last Name")
-FullName = f"{FirstName} {MiddleName} {LastName}"
-print(f"Full Name: {FullName.title()}")
+# # Session 2
+# FirstName = input("First Name: Insert Your First Name")
+# MiddleName = input("Middle Name: Insert Your Middle Name")
+# LastName = input("Last Name: Insert Your Last Name")
+# FullName = f"{FirstName} {MiddleName} {LastName}"
+# print(f"Full Name: {FullName.title()}")
 
 
-# Session 3
+# # Session 3
 PP = ["TPIA", "PTRO", "BRPT"]
-print(PP)
-PP.append("BREN")
-print(PP)
-PP.insert(1, "CUAN")
-text_1 = f"My fav stock is {PP[2]}"
-text_2 = f"My second fav stock is {PP[-1].upper()}"
-print(PP)
-print(text_1)
-print(text_2)
-PP.remove("BRPT")
-print(PP)
-PP.sort()
-print(PP)
-PP.sort(reverse=True)
-print(PP)
-print(sorted(PP))
-print(PP)
-PP.reverse()
-print(PP)
+# print(PP)
+# PP.append("BREN")
+# print(PP)
+# PP.insert(1, "CUAN")
+# text_1 = f"My fav stock is {PP[2]}"
+# text_2 = f"My second fav stock is {PP[-1].upper()}"
+# print(PP)
+# print(text_1)
+# print(text_2)
+# PP.remove("BRPT")
+# print(PP)
+# PP.sort()
+# print(PP)
+# PP.sort(reverse=True)
+# print(PP)
+# print(sorted(PP))
+# print(PP)
+# PP.reverse()
+# print(PP)
 print(len(PP))
 
-PPS = ["PTRO", "BREN", "CUAN", "TPIA", "BRPT"]
-print(PPS)
-print("Rank PP's Stock here:")
-rank_1 = int(input("PTRO: "))
-rank_2 = int(input("BREN: "))
-rank_3 = int(input("CUAN: "))
-rank_4 = int(input("TPIA: "))
-rank_5 = int(input("BRPT: "))
-if rank_1 == 1:
-    print("Good Boy")
-else:
-    print("Bad Boy, should've done number 1")
+# PPS = ["PTRO", "BREN", "CUAN", "TPIA", "BRPT"]
+# print(", ".join(PPS))
+# for PPS in PPS:
+#     print(PPS)
+# print("Rank PP's Stock here:")
+# rank_1 = int(input("PTRO: "))
+# rank_2 = int(input("BREN: "))
+# rank_3 = int(input("CUAN: "))
+# rank_4 = int(input("TPIA: "))
+# rank_5 = int(input("BRPT: "))
+# if rank_1 == 1:
+#     print("Good Boy")
+# else:
+#     print("Bad Boy, should've done number 1")
 
-if rank_2 == 2:
-    print("Good Boy")
-else:
-    print("Bad Boy, should've done number 2")
+# if rank_2 == 2:
+#     print("Good Boy")
+# else:
+#     print("Bad Boy, should've done number 2")
 
-if rank_3 == 3:
-    print("Good Boy")
-else:
-    print("Bad Boy, should've done number 3")
+# if rank_3 == 3:
+#     print("Good Boy")
+# else:
+#     print("Bad Boy, should've done number 3")
 
-if rank_4 == 4:
-    print("Good Boy")
-else:
-    print("Bad Boy, should've done number 4")
+# if rank_4 == 4:
+#     print("Good Boy")
+# else:
+#     print("Bad Boy, should've done number 4")
 
-if rank_5 == 5:
-    print("Good Boy")
-else:
-    print("Bad Boy, should've done number 5")
+# if rank_5 == 5:
+#     print("Good Boy")
+# else:
+#     print("Bad Boy, should've done number 5")
 
 
-# Session 4
+# # Session 4
+# PP = ["BRPT", "TPIA", "BREN", "PTRO", "CUAN"]
+# for stock in PP:
+#     if stock == "CUAN":
+#         break
+#     print("PP's stock:", "".join(stock))
+
+# Session 5
